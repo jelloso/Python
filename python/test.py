f6b6af5cc6acc75de790c69dbacd4eb1e39d4339
Jeremiah Elloso <jelloso@zendesk.com>
2019-08-19 19:32:07 +1000

    adding new exercises

--- python/test.py
+++
@@ -1,7 +0,0 @@
-from requests import get
-import matplotlib.pyplot as plt
-from dateutil import parser
-url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getallmeasurements/1615966'
-weather = get(url).json()
-#print(weather)
-print(weather['items'])
